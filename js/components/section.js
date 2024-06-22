a63dad1e0624fd5378a3bc5220b74b14e3d21cd5
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-21 23:27:19 -0500

    feat: :sparkles: icons eventListener added
    the check and the trash was added eventListener

--- js/components/section.js
+++ js/components/section.js
@@ -6,30 +6,79 @@ export const writeTasks = async(res) => {
     `;
     res.forEach(elements => {
         if (elements.status === "On hold") {
+            let section__task = document.querySelector(".section__task");
             plantilla += /*html*/`
             <article class="tasks">
-                <p>${elements.task}</p>
+            <p>${elements.task}</p>
             <div class="icons">
-                <img class="check" src="images/check-circle-svgrepo-com.svg" alt="">
-                    <div class="circle__trash">
-                        <img class="trash" src="images/trash-svgrepo-com.svg" alt="">
-                    </div>
-                </div>
+            <img class="check" src="images/check-circle-svgrepo-com.svg" alt="">
+            <div class="circle__trash">
+            <img class="trash" src="images/trash-svgrepo-com.svg" alt="">
+            </div>
+            </div>
             </article>
-            `}else {
+            `;
+            section__task.innerHTML = plantilla
+            
+            let check = document.querySelectorAll(".check");
+            check.forEach(element => {
+                element.addEventListener("click", (e) => {
+                    console.log("se realizo click",e);
+                });
+            })
+            
+            
+            let trash = document.querySelectorAll(".trash");
+            trash.forEach(element => {
+                element.addEventListener("click", (e) => {
+                    console.log("se realizo click",e);
+                })
+
+            })
+        }else {
+            let section__task = document.querySelector(".section__task");
             plantilla += /*html*/`
             <article class="tasks__done">
-                <p class="name__check" >${elements.task}</p>
+            <p class="name__check" >${elements.task}</p>
             <div class="icons">
-                <img class="check" src="images/check-circle-svgrepo-com.svg" alt="">
-                    <div class="circle__trash">
-                        <img class="trash" src="images/trash-svgrepo-com.svg" alt="">
-                    </div>
-                </div>
+            <img class="check" src="images/check-circle-svgrepo-com.svg" alt="">
+            <div class="circle__trash">
+            <img class="trash" src="images/trash-svgrepo-com.svg" alt="">
+            </div>
+            </div>
             </article>
-            `}
+            `;
+            section__task.innerHTML = plantilla
+            
+            let checks = document.querySelectorAll(".check");
+            let trashs = document.querySelectorAll(".trash");
+
+            if (checks && trashs) {    
+            checks.forEach(element => {
+                element.addEventListener("click", (e) => {
+                    console.log("se realizo click",e);
+                });
+            });
+            
+            trashs.forEach(element => {
+                element.addEventListener("click", (e) => {
+                    console.log("se realizo click",e);
+                });
+
+            });
+            }else{
+                let check = document.querySelector(".check");
+                check.addEventListener("click", (e) => {
+                    console.log("se realizo click",e);
+                });
+                
+                let trash = document.querySelector(".trash");
+                trash.addEventListener("click", (e) => {
+                console.log("se realizo click",e);
+                });
+            };
+        }
     });
-    return plantilla;
 }
 
 export const addTask = (input) => {
@@ -45,5 +94,5 @@ export const addTask = (input) => {
         </div>
     </article>
     `;
-    return plantilla 
+    return plantilla;
 }
--- js/main.js
+++ js/main.js
@@ -4,15 +4,31 @@ import {writeTasks, addTask} from "./components/section.js";
 let section__task = document.querySelector(".section__task");
 let input__search = document.querySelector("#input__search");
 
+
+
+
 let data = await getAllList();
 let allTaks = await writeTasks(data);
 
-section__task.innerHTML = allTaks;
 
 input__search.addEventListener("change", (e) => {
     let text = e.target.value;
     input__search.value = null;
     let search = addTask(text);
-    section__task.innerHTML += search;
+    section__task.innerHTML += search;  
+    console.log(section__task.value)
+    let check = document.querySelector(".check");
+    check.addEventListener("click", (e) => {
+        console.log("se realizo click",e);
+        });
+
+    let trash = document.querySelector(".trash");
+    trash.addEventListener("click", (e) => {
+        console.log("se realizo click",e);
+    })  
     console.log(search);
+});
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+
 })
